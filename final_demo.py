e91ac4b9811e00c6be1b0c6fb45d5d501ad4e91c
Vidyadhar Thatte <vd_thatte@yahoo.com>
2016-11-30 22:20:33 -0500

    added project files

---
+++ final_demo.py
@@ -0,0 +1,93 @@
+import os
+import sys
+import json
+
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+print("AYY LMAO")
+
+@app.route('/', methods=['GET'])
+def verify():
+
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "test_token":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+    # Recieve your package from facebook
+
+    data = request.get_json()
+    
+    # print(data)
+    
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    print(sender_id)
+                    print(message_text)
+
+                    # responding to your user
+                    send_message(sender_id, message_text)
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    # Prepare your package
+
+    params = {
+        "access_token": "EAAYwt0UP4psBAGANjPbNrkCg3WZCrz15ZAkBZB1fchNPzj3d8F4tZBs6du6kB0b5TwIZCNVUJZAuy3BnAi9fHSuJuuR2ZA0hNrx2IRmW4hH62C1ZBarQaOSMMtad8JneBf0uV3GS3rITztTk0l4NCBRr5xkeyyaEBlE6pBB3RQe1HAZDZD"
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+        	# add the text you want to send here
+            "text": message_text
+        }
+    })
+
+    # Send the package to facebook with the help of a POST request
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+
+    # do this to check for errors
+    if r.status_code != 200:
+    	print("something went wrong")
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import json
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+
+# ADD YOUR MAGICAL CODE HERE 
+
+
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
